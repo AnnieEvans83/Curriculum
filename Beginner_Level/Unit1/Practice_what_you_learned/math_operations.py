1549bd2f22e85cf1f8faf0c3188bf093bb41c1e5
Joyce Rigelo <joyce@velexi.com>
2017-06-01 22:51:05 -0500

    add practing exercises

---
+++ Beginner_Level/Unit1/Practice_what_you_learned/math_operations.py
@@ -0,0 +1,26 @@
+"""
+Math Operations
+"""
+
+# 1. Set sum_up equal to the sum of a small and a big number
+sum_up = 0
+
+print("The sum is: ", sum_up)
+
+
+# 2. Set subtract_this equal to the subtraction of two big numbers
+subtract_this = 0
+
+print("The subtraction is: ", subtract_this)
+
+
+# 3. Set divide_me equal to the division of two small numbers
+divide_me = 0
+
+print("The division is: ", divide_me)
+
+
+# 3. Set my_multiplication equal to the multiplication of one and a big number
+my_multiplication = 0
+
+print("The multiplication is: ", my_multiplication)
---
+++ Beginner_Level/Unit1/Practice_what_you_learned/variables.py
@@ -0,0 +1,16 @@
+"""Variables"""
+
+# To set the value 3 to the variable distance we code
+distance = 3
+
+# 1. Set the variable speed to the value 5.
+speed =
+
+# 2. Set the variable fast_speed to the value True.
+
+# 3. Change the value in the variable below to 1.23
+# What will be printed now?
+
+a_variable = 3.4
+
+print(a_variable)
